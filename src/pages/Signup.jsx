a125b1c1e91c6321e1ae08fb8c8413a86c61ce9a
Harniman-Singh <76156964+Harniman-Singh@users.noreply.github.com>
2024-05-01 10:25:12 +0530

    Update Signup.jsx

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -6,7 +6,7 @@ import signupImg from "../assets/Images/signup.webp"
 function Signup({ setIsLoggedIn }) {
   return (
     <Template
-      title="Join the millions learning to code with StudyWithNiman for free"
+      title="Join the millions learning for free"
       desc1="Build skills for today, tomorrow, and beyond."
       desc2="Education to future-proof your career."
       image={signupImg}
