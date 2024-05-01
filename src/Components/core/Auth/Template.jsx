aa9b21ac6d177672aba5554f5ab45b102bd397f1
Harniman-Singh <76156964+Harniman-Singh@users.noreply.github.com>
2024-05-01 10:28:59 +0530

    Update Template.jsx

--- src/Components/core/Auth/Template.jsx
+++ src/Components/core/Auth/Template.jsx
@@ -16,7 +16,7 @@ function Template({ title, desc1, desc2, image, formType }) {
   return (
     <div className='flex w-11/12 max-w-[1160px] py-8 mx-auto gap-x-12 justify-between gap-y-0'>
       
-    <div className='w-11/12 max-w-[450px]'>
+    <div className='w-11/12 max-w-[460px]'>
     <h1 className='text-richblack-5 font-semibold text-[1.875rem] leading-[2.375rem]'>{title}</h1>
     <p className='text-[1.125rem] leading-[1.625rem] mt-4'>
       <span className='text-richblack-100'>{desc1}</span>
